578e187e37105776f67674cf77b554d88f6f647c
Lanesra712 <danvic96@hotmail.com>
2020-02-16 20:46:27 +0800

    Add sample code about base usage of template

--- src/components-guide/src/app/app.module.ts
+++ src/components-guide/src/app/app.module.ts
@@ -1,18 +1,47 @@
+/**
+ * Angular 根模块，引导应用启动
+ */
+
+/**
+ * BrowserModule: 浏览器解析模块
+ * NgModule: Angular 核心模块
+ * AppRoutingModule: 路由模块
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+/**
+ * AppComponent: 应用的根组件
+ * ProductListComponent: product-list 组件
+ */
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+/**
+ * 引入表单模块，从而使用双向数据绑定
+ */
+import { FormsModule } from '@angular/forms';
+
+/**
+ * @NgModule() 装饰器，用来接收一个元数据对象，用来描述这个根模块
+ */
 @NgModule({
+  // 声明当前应用需要使用的组件、指令、管道
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
+  // 引入当前模块能够正常运行时所需要依赖的别的模块
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  // 模块所提供的全局服务，可以被应用中任何部门使用
   providers: [],
+  // 应用的主视图 - 根组件，用来引导 AppModule 启动应用，是应用中其它视图的宿主
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/components-guide/src/app/product-list/product-list.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * @Component() 装饰器，通过一个元数据对象来完成组件和视图间的关联，以及完成对于该组件的配置
+ */
+@Component({
+  // 选择器，当在 html 页面上使用了该选择器所对应的标签值，则会在使用的位置上创建并插入这个组件的一个实例
+  selector: 'app-product-list',
+  // 组件所对应的 html 模板文件地址
+  templateUrl: './product-list.component.html',
+  // 组件视图所特有的 css 样式文件地址
+  styleUrls: ['./product-list.component.scss']
+})
+
+/**
+ * 组件类，为视图提供各种服务
+ */
+export class ProductListComponent implements OnInit {
+
+  //  标题
+  public title = '我是 title 属性值';
+
+  public styleProperty = '<b>我是包含 html 标签的属性</b>';
+
+  // 字体颜色
+  public fontColor = 'red';
+
+  // 链接地址
+  public url = 'https://yuiter.com';
+
+  // 姓名
+  public name: string;
+
+  public msg: string;
+
+  public refMsg: string;
+
+  public inlineStyle: {};
+
+  public currentStyles: {};
+
+  public flag = true;
+
+  public products = [{
+    'name': 'lalala',
+    'price': '$200'
+  }, {
+    'name': 'hehehe',
+    'price': '$400'
+  }, {
+    'name': 'wuwuwu',
+    'price': '$120'
+  }, {
+    'name': 'xixi',
+    'price': '$570'
+  }];
+
+  public config = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.setInlineStyle();
+    this.setCurrentStyles();
+  }
+
+  getUser() {
+    alert('111111111');
+  }
+
+  getMsg(event: KeyboardEvent) {
+    console.log(event);
+    this.msg = (event.target as HTMLInputElement).value;
+  }
+
+  getRefMsg(msg: string) {
+    this.refMsg = msg;
+  }
+
+  setInlineStyle() {
+    this.inlineStyle = {
+      'text-red': true,
+      'bg-blue': false,
+    };
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'font-style': 'italic',
+      'font-weight': 'bold',
+      'font-size': '24px'
+    };
+  }
+
+  removeElement() {
+    this.flag = false;
+  }
+
+  trackByIndex(index: number, item: any): string {
+    return item.price;
+  }
+
+  addProduct() {
+    this.products = [{
+      'name': 'lalala',
+      'price': '$200'
+    }, {
+      'name': 'hehehe',
+      'price': '$400'
+    }, {
+      'name': 'wuwuwu',
+      'price': '$120'
+    }, {
+      'name': 'xixi',
+      'price': '$570'
+    }, {
+      'name': 'lululu',
+      'price': '$' + (Math.random() * 100).toFixed()
+    }];
+  }
+}
